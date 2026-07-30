8decd7bc082cb8a7a1d2e16214c96847765e9eee
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2026-07-30 12:03:51 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        if the length of the strings are not the same, immediately return false
+        create a hashmap for each string
+        make each character in the string maps to a frequency value
+        anagrams will have the exact same count and frequency of characters
+        """
+
+        if len(s) != len(t):
+            return False
+        
+        hashmap_s, hashmap_t = {}, {}
+
+        for i in range(len(s)):
+            hashmap_s[s[i]] += 1
+            hashmap_t[t[i]] += 1
+        return hashmap_s == hashmap_t
+
+        
+
+        
+        
