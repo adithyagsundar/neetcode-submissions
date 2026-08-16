13f876a8b6fcf0eae0f37a57325c236713ccbf2e
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2026-08-16 15:27:41 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        prefix sum tells you the value of all the ints so far
+        so if we want to take it from a non-prefix then we subtract the prefix sum right before it (i.e. subtracting everything before it which is not included)
+        """
+
+        res = []
+
+        for i, num1 in enumerate(nums):
+            product = 1
+            for j, num2 in enumerate(nums):
+                product = product * j
+            product = product / nums[i]
+            res.append(product)
+        return res
+
+
+
+            
+
