222297e457bb9e5ac0a12575b07e861eb4d06b0b
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2026-08-29 13:49:09 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        ][
+        )]}{[(
+        every opening bracket comes BEFORE a closing bracket
+        if a closing bracket, check if the correspondign open bracket is at the TOP OF THE STACK. this is because we want to make sure that there are NO OTHER brackets in between
+        """
+
+        brackets = []
+
+        pairs = {"}" : "{", 
+                ")" : "(", 
+                "]" : "["}
+
+        for c in s:
+            if c == "(" or c == "[" or c == "{":
+                brackets.append(c)
+                continue
+
+            if not brackets:
+                return False
+
+            if pairs[c] != brackets.pop(): #top of the stack
+                return False
+
+        return len(brackets) == 0
+            
+
+
+
+        
